b8bca52a1f555236f283bee0666700f87bc7315f
Michael Haurholm Sternbo <mich475r@edu.easj.dk>
2017-03-13 14:26:36 +0100

    Lavet methods til at finde alle kunder + hoteller

--- HotelDbEntityFrameWork/Program.cs
+++ HotelDbEntityFrameWork/Program.cs
@@ -10,6 +10,61 @@ namespace HotelDbEntityFrameWork
     {
         static void Main(string[] args)
         {
+            int input;
+
+            Console.WriteLine("\n1. List All Hotels\n2. List All Guests");
+            input = int.Parse(Console.ReadLine());
+                     
+            switch (input)
+            {
+                case 1:
+                    SelectAllHotel();
+                    break;
+                case 2:
+                    SelectAllKunder();
+                    break;
+                default:
+                    Console.WriteLine("Me no understand...");
+                    break;
+            }
+
+            Console.ReadLine();
+
+        }   
+
+        // method
+
+        public static void SelectAllHotel()
+        {
+            using (var context = new HotelContext())
+            {
+                Console.WriteLine(" Alle hoteller");
+
+                var allHotels = context.Hotels;
+
+                foreach (var hotelObj in allHotels)
+                {
+                    Console.WriteLine(hotelObj);
+                }
+
+            }
         }
+
+        public static void SelectAllKunder()
+        {
+            using (var context = new HotelContext())
+            {
+                Console.WriteLine(" Alle kunder");
+
+                var allKunder = context.Guests;
+
+                foreach (var kundeObj in allKunder)
+                {
+                    Console.WriteLine(kundeObj);
+                }
+            }
+        }
+
+
     }
 }
